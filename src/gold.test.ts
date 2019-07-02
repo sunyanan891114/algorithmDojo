4f66c274c2a18fe1d0c0ca1094b2e638fb993f37
Li Yu <liyu@odc.cmbchina.cn>
2019-07-02 14:35:08 +0800

    fix test

--- src/gold.test.ts
+++ src/gold.test.ts
@@ -1,4 +1,4 @@
-import { getMostGold } from "./bag";
+import { getMostGold } from "./gold";
 
 test("calMaxValue should return for", () => {
     const result = getMostGold(3, 10, [1, 2, 3], [1, 2, 3]);
