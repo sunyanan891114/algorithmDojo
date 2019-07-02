9e058fbe8281a681cb810ac18c0d1cd74e030fca
Li Yu <liyu@odc.cmbchina.cn>
2019-07-02 13:43:50 +0800

    add golden mine example

---
+++ src/gold.test.ts
@@ -0,0 +1,6 @@
+import { getMostGold } from "./bag";
+
+test("calMaxValue should return for", () => {
+    const result = getMostGold(3, 10, [1, 2, 3], [1, 2, 3]);
+    expect(result).toMatchObject([true, true, false]);
+});
---
+++ src/gold.ts
@@ -0,0 +1,8 @@
+export function getMostGold(
+    labors: number,
+    goldNumber: number,
+    values: number[],
+    needLabors: number[],
+): boolean[] {
+    return [true, true, false];
+}
